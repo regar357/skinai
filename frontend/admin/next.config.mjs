b955b0d9be309f8ccde5f0b128e86243c4603b11
YJH <kerspective@gmail.com>
2026-04-23 17:51:12 +0900

    Add admin frontend

---
+++ frontend/admin/next.config.mjs
@@ -0,0 +1,19 @@
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {
+    root: __dirname,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig
