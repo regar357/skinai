4a0b721dca60e431002f5bb6fbe0f13d26a361a3
regar357 <regar357@naver.com>
2026-04-30 20:02:33 +0900

    백엔드 파일 구성 세팅

--- backend/admin-service/interfaces/routes/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const adminController = require("../controllers/adminController");
-
-const router = express.Router();
-router.post("/login", adminController.adminLogin);
-router.post("/logout", adminController.adminLogout);
-
-module.exports = router;
