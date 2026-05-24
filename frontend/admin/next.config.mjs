949470047f5a76fbbe4e6216db3b25752a76d544
regar357 <regar357@naver.com>
2026-05-25 03:28:34 +0900

    배포 docker 코드 수정

--- frontend/admin/next.config.mjs
+++ frontend/admin/next.config.mjs
@@ -5,11 +5,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack: {
-    root: __dirname,
-  },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
   images: {
     unoptimized: true,
